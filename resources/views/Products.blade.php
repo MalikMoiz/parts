05e212ca75d32e88023e9ba1155146b6e5eccd3a
MalikMoiz <60312766+MalikMoiz@users.noreply.github.com>
2021-08-09 20:08:31 +0500

    Changes

--- resources/views/Products.blade.php
+++ resources/views/Products.blade.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Spare Parts</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <link rel="stylesheet" href="{{asset('/plugins/fontawesome-free/css/all.min.css')}}">
-    <link rel="stylesheet" href="{{asset('/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
-    <link rel="stylesheet" href="{{asset('/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
-    <link rel="stylesheet" href="{{asset('/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
-    <link rel="stylesheet" href="{{asset('/dist/css/adminlte.min.css')}}">
-    <style>
-      
-    </style>
-  </head>
-  <body class="hold-transition sidebar-mini">
-    @include('sidebar')
+@extends('base')
+
+@section('section')
+    
+
     <div class="content-wrapper">
         <br>
         @include('modal')
@@ -84,40 +71,4 @@
           </div>
     </div>
 
-      
-    <script src="{{asset('/plugins/jquery/jquery.min.js')}}"></script>
-    <script src="{{asset('/plugins/bootstrap/js/bootstrap.bundle.min.js')}}""></script>
-    <script src="{{asset('/plugins/datatables/jquery.dataTables.min.js')}}""></script>
-    <script src="{{asset('/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
-    <script src="{{asset('/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
-    <script src="{{asset('/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
-    <script src="{{asset('/plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
-    <script src="{{asset('/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
-    <script src="{{asset('/plugins/jszip/jszip.min.js')}}"></script>
-    <script src="{{asset('/plugins/pdfmake/pdfmake.min.js')}}"></script>
-    <script src="{{asset('/plugins/pdfmake/vfs_fonts.js')}}"></script>
-    <script src="{{asset('/plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
-    <script src="{{asset('/plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
-    <script src="{{asset('/plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
-    <script src="{{asset('/dist/js/adminlte.min.js')}}"></script>
-    <script src="{{asset('/dist/js/demo.js')}}"></script>
-
-    <script>
-      $(function () {
-        $("#example1").DataTable({
-          "responsive": true, "lengthChange": false, "autoWidth": false,
-          "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-        $('#example2').DataTable({
-          "paging": true,
-          "lengthChange": false,
-          "searching": false,
-          "ordering": true,
-          "info": true,
-          "autoWidth": false,
-          "responsive": true,
-        });
-      });
-    </script>
-  </body>
-</html>
+    @endsection
